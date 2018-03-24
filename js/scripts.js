93d86fc17cd804e411d32868ed618317bc0b546e
bdav87 <bdav87@gmail.com>
2018-03-24 10:23:22 -0500

    First commit of page

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/* 
+** Write all website functions within the event listener callback.
+** This keeps the script from blocking the page from loading,
+** and keeps your variables out of the global namespace.
+*/
+document.addEventListener('DOMContentLoaded', function(){
+
+    /* 
+    ** An IIFE (immediately invoked function expression) to add
+    ** a click event to the logo area.
+    */
+   /* 
+    (function greetNoobs(){
+        var logo = document.querySelector('#logoArea');
+        logo.addEventListener('click', function(){
+            alert('Hello Code Noobs!');
+        })
+    })();
+    */
+})
