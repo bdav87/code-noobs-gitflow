023c01c69c6ee568521eee37488c9f1d39fa7987
bdav87 <bdav87@gmail.com>
2018-03-24 11:07:42 -0500

    Preparing repo for lesson

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,36 @@ document.addEventListener('DOMContentLoaded', function(){
     ** An IIFE (immediately invoked function expression) to add
     ** a click event to the logo area.
     */
-   /* 
+    /*
     (function greetNoobs(){
-        var logo = document.querySelector('#logoArea');
+        var logo = document.querySelector('#logo-area');
         logo.addEventListener('click', function(){
             alert('Hello Code Noobs!');
         })
+    })();
+    */
+    /*
+   (function gsearch(){
+        var searchbox = document.querySelector('#search-form');
+        searchbox.addEventListener('submit', function(e){
+            e.preventDefault();
+            var searchterm = document.querySelector('#g-search');
+            searchterm = searchterm.value;
+            window.open(`https://www.google.com/search?q=${searchterm}`, '_blank');
+        })
+    })();
+    */
+    /*
+    (function howdy(){
+        var namebox = document.querySelector('#name-form');
+        namebox.addEventListener('submit', function(e){
+            e.preventDefault();
+            var name = document.querySelector('#name-greet').value;
+            var response = document.querySelector('#response-area');
+            response.style.display = 'block';
+            response.value = "";
+            response.value = `Howdy ${name}! Do you have any tacos?`;
+        });
     })();
     */
 })
